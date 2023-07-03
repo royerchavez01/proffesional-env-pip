93aaf5c1ba3a56b043623cef35675e6b7bc9a281
royer01 <royer01@MSI.localdomain>
2023-07-02 23:26:19 -0500

    Se ingresa country y se genera gráfico con nombre del país

--- app/charts.py
+++ app/charts.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 
-def generate_bar_chart(labels, values):
+def generate_bar_chart(name, labels, values):
   fig, ax = plt.subplots()
   ax.bar(labels, values)
-  plt.savefig('bar.png')
+  plt.savefig(f'./imgs/{name}.png')
   plt.close()
 
 def generate_pie_chart(labels, values):
