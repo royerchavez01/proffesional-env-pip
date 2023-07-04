28cb57a2f44dce635f665dfd042f83593b5f54b2
royer01 <royer01@MSI.localdomain>
2023-07-04 12:26:15 -0500

    añadimos pandas al requirement.txt y usamos pandas en main.py

--- app/main.py
+++ app/main.py
@@ -8,19 +8,25 @@ from name-package import name-module'''
 import charts
 import read_csv
 import utils
+import pandas as pd
 
 
 def run():
-  data = read_csv.read_csv('world_data.csv')
-
+  '''
   data = list(filter(lambda item: item['Continent'] == 'South America', data))
   labels = list(map(lambda x: x['Country/Territory'], data))
   values = list(map(lambda x: x['World Population Percentage'], data))
-  charts.generate_pie_chart(labels, values)
- 
+  '''
 
-  #MOSTRAR POBLACION DE UN PAÍS CON EL TIEMPO
+  df = pd.read_csv('world_data.csv')  # Es lo mismo que read_csv.py
+  df = df[df['Continent'] == 'Africa']  # Es lo mismo que la linea de filter
+
+  labels = df['Country/Territory'].values # Countries o labels
+  values = df['World Population Percentage'].values # percentages o values
+  charts.generate_pie_chart(labels, values)
   
+   #MOSTRAR POBLACION DE UN PAÍS CON EL TIEMPO
+  data = read_csv.read_csv('world_data.csv')
   country = input('Type country => ')
 
   result = utils.population_by_country(data, country)
